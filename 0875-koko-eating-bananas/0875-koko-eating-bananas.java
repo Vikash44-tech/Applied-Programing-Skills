16dec1288571718bef36c1f1004342a239d57b22
Vikash44-tech <vtu25088@veltech.edu.in>
2026-03-08 10:36:48 +0530

    Time: 11 ms (57.98%), Space: 47.8 MB (86.1%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+     public static boolean koko(int[] piles, int h, int k){
+        for(int i=0;i<piles.length;i++){
+            int upper = piles[i] / k;
+            if(piles[i] % k != 0){
+                upper++;
+            }
+            h -= upper;
+            if(h < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 1000000000;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(koko(piles,h,mid)){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
