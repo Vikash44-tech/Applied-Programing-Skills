5a7aa300833a6a84cca37ad41a177dea8540bfed
Vikash44-tech <vtu25088@veltech.edu.in>
2026-03-18 11:02:15 +0530

    Time: 81 ms (17.2%), Space: 80.7 MB (97.17%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static boolean isPossible(int[] nums, int n, int limit){
+        for(int i=0;i<nums.length;i++){
+            int val = nums[i];
+            int temp = val / limit;
+            if(val % limit != 0){
+                temp++;
+            }
+            n-=temp;
+
+            if(n < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int minimizedMaximum(int n, int[] quantities) {
+        int l = 1,r = Integer.MAX_VALUE;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(isPossible(quantities,n,mid)){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
