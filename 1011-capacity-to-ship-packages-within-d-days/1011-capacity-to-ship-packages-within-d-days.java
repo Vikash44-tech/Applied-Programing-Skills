4206c6646ea69a4e193867f17954bf094ce3864b
Vikash44-tech <vtu25088@veltech.edu.in>
2026-03-18 10:03:31 +0530

    Time: 10 ms (85.83%), Space: 50.3 MB (31.95%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static boolean isPossible(int[] nums, int days, int k){
+        int temp = 0;
+        for(int i=0;i<nums.length;i++){
+            int val = nums[i];
+            
+            if(val > k){
+                return false;
+            }
+            
+            if(temp + val > k){
+                days -= 1;
+                temp = 0;
+            }
+            
+            temp += val;
+            
+            if(days <= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+    int l = 1, r = Integer.MAX_VALUE;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(isPossible(weights,days,mid)){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
